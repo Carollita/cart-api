62104e7b8121ba6ae1071f6543af6aa06db0ea04
Carolina <carolsalgadoguadanhini@gmail.com>
2022-10-05 23:25:55 -0300

    add class Address

--- src/main/java/me/dio/cart/model/Address.java
+++ src/main/java/me/dio/cart/model/Address.java
@@ -1,5 +1,18 @@
 package me.dio.cart.model;
 
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Embeddable 
+@NoArgsConstructor
 public class Address {
-    
+    private String zipCode;
+    private String details;
 }

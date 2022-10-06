37b4e8012468dc4efba4a46d28cf1fcad1b45a0c
Carolina <carolsalgadoguadanhini@gmail.com>
2022-10-05 23:10:28 -0300

    add class Restaurant

--- src/main/java/me/dio/cart/model/Restaurant.java
+++ src/main/java/me/dio/cart/model/Restaurant.java
@@ -1,5 +1,30 @@
 package me.dio.cart.model;
 
+import java.util.List;
+
+import javax.persistence.*;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity // an entity represents a table in a relational database
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Restaurant {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL) // a restaurant has several products
+    private List<Product> menu;
+    @Embedded
+    private Address address;
 }

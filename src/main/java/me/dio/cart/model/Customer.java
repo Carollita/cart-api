0d2ee17eb9305811ce4b9ae3aac2ce49f2beb49f
Carolina <carolsalgadoguadanhini@gmail.com>
2022-10-05 23:33:00 -0300

    add class Customer

--- src/main/java/me/dio/cart/model/Customer.java
+++ src/main/java/me/dio/cart/model/Customer.java
@@ -1,5 +1,25 @@
 package me.dio.cart.model;
 
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Customer {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @Embedded
+    private Address address;
 }

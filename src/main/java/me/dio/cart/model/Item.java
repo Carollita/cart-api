68b5601f93cf67a62886197ee648e9c2ed674ac5
Carolina <carolsalgadoguadanhini@gmail.com>
2022-10-06 22:21:26 -0300

    add class Item

--- src/main/java/me/dio/cart/model/Item.java
+++ src/main/java/me/dio/cart/model/Item.java
@@ -1,5 +1,29 @@
 package me.dio.cart.model;
 
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Item {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne // one product, one item
+    private Product product;
+    private int quantity;
+    @ManyToOne // a cart can have multiple items
+    @JsonIgnore
+    private Cart cart;
 }

a2a4fe2f9a087dfec448b5b7aea1a61d8df14588
Carolina <carolsalgadoguadanhini@gmail.com>
2022-10-06 22:19:39 -0300

    add class Product

--- src/main/java/me/dio/cart/model/Product.java
+++ src/main/java/me/dio/cart/model/Product.java
@@ -1,5 +1,30 @@
 package me.dio.cart.model;
 
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Product {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private double unitPrice;
+    @Builder.Default
+    private Boolean productAvailable = true;
+    @ManyToOne // one restaurant, several products
+    @JsonIgnore
+    private Restaurant restaurant;
 }
